203a7cfcfc1d6bc7324879df67d38f3387002dd9
seowon <gaga020530@yonsei.ac.kr>
2024-04-11 18:38:38 +0900

    new

--- 6th/hw03.js
+++ 6th/hw03.js
@@ -1,12 +1,11 @@
-// 3개의 요소를 가지는 2차원 배열 생성
+
 const people = [
-    ['홍길동', '개발자'],
-    ['김철수', '디자이너'],
-    ['이영희', '마케터']
+    ["이서원", "학생"],
+    ["변백현", "가수"],
+    ["이도현", "배우"]
 ];
 
-// 배열 순회하여 출력
 for (let i = 0; i < people.length; i++) {
     const person = people[i];
-    console.log(`이름: ${person[0]}, 직업: ${person[1]}`);
+    console.log(`${person[0]} - ${person[1]}`);
 }
