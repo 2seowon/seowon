c55190729861643967b01e2730f5a5df928f1e40
seowon <gaga020530@yonsei.ac.kr>
2024-04-11 18:32:15 +0900

    4/11

---
+++ 6th/hw02.js
@@ -0,0 +1,9 @@
+function introduce(name, birth, nationality, job) {
+    console.log(`이름: ${name}`);
+    console.log(`생년월일: ${birth}`);
+    console.log(`국적: ${nationality}`);
+    console.log(`직업: ${job}`);
+}
+
+// 함수 호출 (예시)
+introduce('홍길동', '1990년 1월 1일', '대한민국', '개발자');
